9f401681c87e06c71e4c5f0486d3d081fb92f031
Khan Asfi Reza <khanasfireza10@gmail.com>
2021-12-07 07:16:41 +0600

    Created `docs.py`

--- MySocial/MySocial/docs.py
+++ MySocial/MySocial/docs.py
@@ -6,14 +6,17 @@ from drf_yasg.views import get_schema_view
 class Gen(OpenAPISchemaGenerator):
     def get_schema(self, request=None, public=False):
         schema = super().get_schema(request, public)
-        for definition in schema.definitions.keys():
-            if hasattr(schema.definitions[definition]._NP_serializer.Meta, "swagger_example"):
-                examples = schema.definitions[definition]._NP_serializer.Meta.swagger_example
-                for example in examples.keys():
-                    if example in schema.definitions[definition]["properties"]:
-                        schema.definitions[definition]["properties"][example][
-                            "example"
-                        ] = examples[example]
+        try:
+            for definition in schema.definitions.keys():
+                if hasattr(schema.definitions[definition]._NP_serializer.Meta, "swagger_example"):
+                    examples = schema.definitions[definition]._NP_serializer.Meta.swagger_example
+                    for example in examples.keys():
+                        if example in schema.definitions[definition]["properties"]:
+                            schema.definitions[definition]["properties"][example][
+                                "example"
+                            ] = examples[example]
+        except AttributeError:
+            pass
         return schema
 
 
