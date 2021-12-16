9174b3f173f64320ea2b1db3dc44e21beb568945
Khan Asfi Reza <khanasfireza10@gmail.com>
2021-12-16 12:32:49 +0600

    Created `permission.py`

--- MySocial/MySocial/permission.py
+++ MySocial/MySocial/permission.py
@@ -7,9 +7,13 @@ class ProfilePermissionClass(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         # Read permissions are allowed to any request,
         # so we'll always allow GET, HEAD or OPTIONS requests.
-        if (request.method == 'GET' or request.method == 'POST') and request.user.is_authenticated:
+        if (
+            request.method == "GET" or request.method == "POST"
+        ) and request.user.is_authenticated:
             return True
-        elif (request.method == 'PUT' or request.method == 'DELETE') and request.user.is_authenticated:
+        elif (
+            request.method == "PUT" or request.method == "DELETE"
+        ) and request.user.is_authenticated:
             return obj.user == request.user
         return False
 
@@ -17,25 +21,28 @@ class ProfilePermissionClass(permissions.BasePermission):
 class IsAdminOrPostOrGet(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         # allow all POST requests
-        if request.method == 'POST':
+        if request.method == "POST":
             return True
 
         # Otherwise, only allow authenticated requests
 
         return (
-                (request.user.is_authenticated and
-                 request.user == obj.user) or
-                request.user.is_admin or
-                request.user.is_staff
+            (request.user.is_authenticated and request.user == obj.user)
+            or request.user.is_admin
+            or request.user.is_staff
         )
 
 
 class IsAuthenticatedOrGet(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         # allow all POST requests
-        if request.method == 'GET':
+        if request.method == "GET":
             return True
 
         # Otherwise, only allow authenticated requests
 
-        return request.user.is_authenticated or request.user.is_admin or request.user.is_staff
+        return (
+            request.user.is_authenticated
+            or request.user.is_admin
+            or request.user.is_staff
+        )
