5ad4219215f157b18dc33bc88df431bb38378e5e
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-01-13 06:45:43 +0600

    Created `routing.py`

---
+++ MySocial/MySocial/routing.py
@@ -0,0 +1,16 @@
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.urls import path, re_path
+
+# from Chat.consumers import OnlineOfflineConsumer, InboxConsumer, MessageConsumer, GetOnlineConsumer
+
+application = ProtocolTypeRouter({
+    'websocket': AuthMiddlewareStack(
+        URLRouter([
+            # path('ws/status/', OnlineOfflineConsumer),
+            # path('ws/inbox/', InboxConsumer),
+            # path('ws/chat/<username>/', MessageConsumer),
+            # re_path('ws/onlineusers/', GetOnlineConsumer),
+        ])
+    ),
+})
