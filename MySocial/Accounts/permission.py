c01cf27b2577110d3f38ce0b3bccf264901f5244
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-01-21 08:08:11 +0600

    Created `permission.py`

--- MySocial/Accounts/permission.py
+++ MySocial/Accounts/permission.py
@@ -4,7 +4,7 @@ from rest_framework import permissions
 class IsPostOrIsAuthenticated(permissions.BasePermission):
 
     def has_permission(self, request, view):
-        # allow all POST requests
+        # Allow Only Create
         if request.method == 'POST':
             return True
 
