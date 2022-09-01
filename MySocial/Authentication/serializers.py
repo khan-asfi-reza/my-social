1e5286434d7e0cbd60de8b36220604e2234307a4
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-09-01 12:55:21 +0600

    Serializer update

--- MySocial/Authentication/serializers.py
+++ MySocial/Authentication/serializers.py
@@ -1,21 +1,16 @@
+from Accounts.serializers import UserSerializerPublic
 from rest_framework import serializers
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer as BaseTokenObtainSerializer
+from rest_framework_simplejwt.serializers import (
+    TokenObtainPairSerializer as BaseTokenObtainSerializer,
+)
 from rest_framework_simplejwt.settings import api_settings
 from rest_framework_simplejwt.tokens import RefreshToken
 
-from Accounts.serializers import UserSerializerPublic
-
 
 class TokenObtainPairSerializer(BaseTokenObtainSerializer):
-
     class Meta:
-        read_only_fields = [
-            "auth_user"
-        ]
-        swagger_example = {
-            "username": "user",
-            "password": "123"
-        }
+        read_only_fields = ["auth_user"]
+        swagger_example = {"username": "user", "password": "123"}
 
     @classmethod
     def get_token(cls, user):
@@ -24,8 +19,8 @@ class TokenObtainPairSerializer(BaseTokenObtainSerializer):
     def validate(self, attrs):
         data = super().validate(attrs)
         refresh = self.get_token(self.user)
-        data['refresh'] = str(refresh)
-        data['access'] = str(refresh.access_token)
+        data["refresh"] = str(refresh)
+        data["access"] = str(refresh.access_token)
         return data
 
     def update(self, instance, validated_data):
@@ -42,21 +37,21 @@ class TokenRefreshSerializer(serializers.Serializer):
     auth_user = UserSerializerPublic(read_only=True, required=False, write_only=False)
 
     class Meta:
-        read_only_fields = [
-            "auth_user",
-            "access"
-        ]
+        read_only_fields = ["auth_user", "access"]
 
     def validate(self, attrs):
         request = self.context["request"]
 
-        refresh = RefreshToken(attrs['refresh'])
+        refresh = RefreshToken(attrs["refresh"])
 
-        data = {'access': str(refresh.access_token), "auth_user": {
-            "uuid": request.user.uuid,
-            "full_name": request.user.get_full_name(),
-            "username": request.user.username
-        }}
+        data = {
+            "access": str(refresh.access_token),
+            "auth_user": {
+                "uuid": request.user.uuid,
+                "full_name": request.user.get_full_name(),
+                "username": request.user.username,
+            },
+        }
         if api_settings.ROTATE_REFRESH_TOKENS:
             if api_settings.BLACKLIST_AFTER_ROTATION:
                 try:
@@ -70,7 +65,7 @@ class TokenRefreshSerializer(serializers.Serializer):
             refresh.set_jti()
             refresh.set_exp()
 
-            data['refresh'] = str(refresh)
+            data["refresh"] = str(refresh)
 
         return data
 
