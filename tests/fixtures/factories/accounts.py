c7e26f935018494bc82654d19d49edd8c8a50485
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-09-05 02:39:18 +0600

    Account test factory

---
+++ tests/fixtures/factories/accounts.py
@@ -0,0 +1,21 @@
+from faker import Faker
+from factory.django import DjangoModelFactory
+from Accounts.models import User
+from pytest_factoryboy import register
+
+faker = Faker()
+
+
+class UserFactory(DjangoModelFactory):
+    username = faker.user_name()
+    first_name = faker.first_name()
+    last_name = faker.last_name()
+    password = faker.uuid4(str)
+    phone_number = faker.phone_number()
+    email = faker.email()
+
+    class Meta:
+        model = User
+
+
+register(UserFactory, "users")
