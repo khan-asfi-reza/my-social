1f160f1858a5915101dde069a7d17f5c1ccb95c8
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-10-02 15:02:29 +0600

    ✨ Create | Modification `client.py`

--- Storage/client.py
+++ Storage/client.py
@@ -1,67 +1,74 @@
-import os
-
 import requests
-from django.conf import settings
-from django.core.files import File
-from django.core.files.storage import Storage
-from django.utils.deconstruct import deconstructible
+
+from Storage.exceptions import StorageException
+from Storage.file.utils import get_url_for_client
 
 
-def upload_file(file):
-    try:
-        STORAGE_API = settings.STORAGE_API
-        url = STORAGE_API + "/file/upload/"
-        response = requests.post(url, files={"file": file})
-        return response.json()
-    except AttributeError:
-        raise Exception("Storage API Not Set in Django Settings")
+def exception_decorator(func):
+    def inner(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except requests.exceptions.HTTPError or requests.exceptions.RequestException:
+            raise StorageException("Storage API Request error, please ensure the storage service is working")
+
+    return inner
 
 
-def get_file(file_id):
-    STORAGE_API = settings.STORAGE_API
-    url = STORAGE_API + f"/file/{file_id}"
+@exception_decorator
+def upload_file(api_endpoint: str, file: str) -> dict[str, str | int]:
+    """
+    Uploads File to Storage Service
+    Args:
+        api_endpoint: str, File Storage API Endpoint
+        file: File[bytes | bytes array], File to be Uploaded
+    """
+    url = get_url_for_client(api_endpoint, "upload")
+    response = requests.post(url, files={"file": file})
+    return response.json()
+
+
+@exception_decorator
+def get_file(api_endpoint: str, file_id: str) -> dict[str, str | int]:
+    """
+    Get File
+    """
+    url = get_url_for_client(api_endpoint, file_id)
     response = requests.get(url)
     return response.content
 
 
-@deconstructible
-class MySocialStorage(Storage):
+@exception_decorator
+def delete_file(api_endpoint: str, file_id: str) -> dict[str, str | int]:
+    """
+    Deletes File
+    """
+    url = get_url_for_client(api_endpoint, file_id)
+    response = requests.delete(url)
+    return response.json()
 
-    def _open(self, name, mode='rb'):
-        file = get_file(name)
-        return File(file=file)
 
-    def _save(self, name, content):
-        if not hasattr(content, 'seekable') or content.seekable():
-            content.seek(0, os.SEEK_SET)
-        file = upload_file(content)
-        return file["id"]
+@exception_decorator
+def get_file_details(api_endpoint: str, file_id: str) -> dict[str, str | int]:
+    """
+    Gets file details
+    """
+    url = get_url_for_client(api_endpoint, "details", file_id)
+    response = requests.get(url)
+    data = response.json()
+    return data
 
-    def path(self, name):
-        STORAGE_API = settings.STORAGE_API
-        return STORAGE_API + f"/{name}"
 
-    def delete(self, name):
-        pass
+@exception_decorator
+def get_file_details_by_key(api_endpoint: str, file_id: str, key: str, default=None) -> str | int | None:
+    """
+    Return details field
+    """
+    details = get_file_details(api_endpoint, file_id)
+    return details.get(key, default)
 
-    def exists(self, name):
-        pass
 
-    def listdir(self, path):
-        pass
-
-    def size(self, name):
-        return 512
-
-    def url(self, name):
-        STORAGE_API = settings.STORAGE_API
-        return STORAGE_API + f"/api/{name}"
-
-    def get_accessed_time(self, name):
-        pass
-
-    def get_created_time(self, name):
-        pass
-
-    def get_modified_time(self, name):
-        pass
+def file_api_url(api_endpoint: str, *args):
+    """
+    Formats api endpoints
+    """
+    return get_url_for_client(api_endpoint, *args)
